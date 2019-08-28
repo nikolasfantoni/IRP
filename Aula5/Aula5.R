f40a91dceca15f0496140d3be6ea4f03bd55d801
nikolasfantoni <nikolasfantoni@gmail.com>
2019-08-28 03:19:21 -0300

    Aula7+correcoes

--- Aula5/Aula5.R
+++ Aula5/Aula5.R
@@ -1,10 +1,15 @@
+#Universidade Federal de Minas Gerais
+#Introducao ao Reconhecimento de Padroes
+#Nikolas Dias Magalhaes Fantoni
 #AULA 5 - Base Heart
+#2019/2
 
 #Limpando o ambiente
 rm(list=ls())
 
 #funcao estimativa densidade para n variáveis
-pdfnvar <- function(x, m, K, n) {(1/(sqrt((2*pi)^(n)*(det(K)))))*exp(-0.5*(t(x-m)%*%(solve(K))%*%(x-m)))}
+pdfnvar <- function(x, m, K, n) {(1/(sqrt((2*pi)^(n)*(det(K))))) * 
+    exp(-0.5 * (t(x-m) %*% (solve(K)) %*% (x-m)))}
 
 #percentual amostra
 per <- 0.9
@@ -21,15 +26,14 @@ iseq1 <- sample(length(heartc1[,1]))
 iseq2 <- sample(length(heartc2[,1]))
 trainc1 <- heartc1[iseq1[1:(per*length(iseq1))],]
 trainc2 <- heartc2[iseq2[1:(per*length(iseq2))],]
-test <- rbind(heartc1[iseq1[(per*length(iseq1)+1):length(iseq1)],], heartc2[iseq2[(per*length(iseq2)+1):length(iseq2)],])
+test <- rbind(heartc1[iseq1[(per*length(iseq1)+1):length(iseq1)],], 
+              heartc2[iseq2[(per*length(iseq2)+1):length(iseq2)],])
 
 #media e desvio padrão
-u1 <- rbind(mean(trainc1[,1]), mean(trainc1[,2]), mean(trainc1[,3]), mean(trainc1[,4]), mean(trainc1[,5]),
-            mean(trainc1[,6]), mean(trainc1[,7]), mean(trainc1[,8]), mean(trainc1[,9]), mean(trainc1[,10]),
-            mean(trainc1[,11]), mean(trainc1[,12]), mean(trainc1[,13]))
-u2 <- rbind(mean(trainc2[,1]), mean(trainc2[,2]), mean(trainc2[,3]), mean(trainc2[,4]), mean(trainc2[,5]),
-            mean(trainc2[,6]), mean(trainc2[,7]), mean(trainc2[,8]), mean(trainc2[,9]), mean(trainc2[,10]),
-            mean(trainc2[,11]), mean(trainc2[,12]), mean(trainc2[,13]))
+for (m in 1:13){
+  u1 <- rbind(u1, mean(trainc1[,m]))
+  u2 <- rbind(u2, mean(trainc2[,m]))
+}
 
 #covariancias e coeficientes de correlação
 cov1 <- cov(trainc1[,1:13])
@@ -44,3 +48,4 @@ for (i in 1:length(test[,1])){
 }
 
 acertoporcento <- 100 - erro/length(test[,1])*100
+cat(acertoporcento,"\n")
---
+++ Aula6/Aula6.R
@@ -0,0 +1,65 @@
+#AULA 6 - Base Spam
+
+#Limpando o ambiente
+rm(list=ls())
+
+#Adicionando biblioteca
+library(caret)
+
+#Funcao estimativa densidade para n variáveis
+pdfnvar <- function(x, m, K, n) {
+  if (det(K) == 0) 999999999 else (1/(sqrt((2*pi)^(n)*(det(K)))))*exp(-0.5*(t(x-m)%*%(solve(K))%*%(x-m)))
+  }
+
+#Lendo os dados
+spam <- read.csv( "spambase.data", sep=",",header = FALSE)
+ic1 <- which(spam$V58 == 1)
+ic2 <- which(spam$V58 == 0)
+spamc1 <- spam[ic1,]
+spamc2 <- spam[ic2,]
+
+#Criando os folds que separao os dados
+fl1 <- createFolds(spamc1[,1], k = 10, list = TRUE, returnTrain = FALSE)
+fl2 <- createFolds(spamc1[,1], k = 10, list = TRUE, returnTrain = FALSE)
+
+#Looping para os 10 testes
+acuraciaacumulada <- NULL
+for (j in 1:10){
+  test <- rbind(spamc1[fl1[[j]],],spamc1[fl1[[j]],])
+  trainc1 <- spamc1[-fl1[[j]],]
+  trainc2 <- spamc2[-fl1[[j]],]
+    
+  #Calculando as medias
+  u1<- NULL
+  u2<- NULL
+  for (m in 1:57){
+  u1 <- rbind(u1, mean(trainc1[,m]))
+  u2 <- rbind(u2, mean(trainc2[,m]))
+  }
+  
+  #Covariancias dos dados
+  cov1 <- cov(trainc1[,1:57])
+  cov2 <- cov(trainc2[,1:57])
+  
+  #Testando o algoritmo
+  erro <- 0
+  for (i in 1:length(test[,1])){
+    l <- t(test[i,1:57])
+    f1 <- pdfnvar(l, u1 , cov1, 57)
+    f2 <- pdfnvar(l, u2 , cov2, 57)
+    if (f2 == 0){
+      c <- 1
+    } else {
+    c <- if ((f1/f2 >= 1) == TRUE) 1 else 0
+    if ((c-test[i,58]) != 0) erro <- erro +1
+    }
+  }
+ 
+  #Obtendo a acuracia 
+  acerto <- 1 - erro/length(test[,1])
+  acuraciaacumulada <- c(acuraciaacumulada, acerto)
+}
+
+#Imprimindo a saida
+cat("\n acuracia: [", acuraciaacumulada, "]\n", "media: ", 
+    mean(acuraciaacumulada)*100,"%", "\n", "desvio: ", sd(acuraciaacumulada)*100,"%","\n\n")
