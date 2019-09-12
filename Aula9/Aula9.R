f2767d96ad5a1721a2009a423df12f3fc36f4201
nikolasfantoni <nikolasfantoni@gmail.com>
2019-09-11 22:01:18 -0300

    finalizada aula 9

--- Aula9/Aula9.R
+++ Aula9/Aula9.R
@@ -8,93 +8,169 @@
 rm(list=ls())
 
 #Adicionando biblioteca
+library('caret')
 library('mlbench')
-
-#Funcao para estimativa da densidade de 2 variaveis
-pdf2var<-function(x,y,u1,u2,s1,s2,p) {(1/(2*pi*s1*s2*sqrt(1-(p^2))))*
-    exp((-(1)/(2*(1-(p^2))))*((((x-u1)^2)/((s1)^2))+(((y-u2)^2)/((s2)^2))-
-                                ((2*p*(x-u1)*(y-u2))/(s1*s2))))
-}
-
-#Funcao de distancia euclidiana
-eucdist <- function(x,y){
-  sqrt(sum((x-y)^2))
-}
+library('plot3D')
 
 #Criando dados
 nc <- 1000
 X<-mlbench.spirals(nc,cycles=1, sd=0.05)
 classes <- 2
+X <- cbind(X$x, X$classes)
+plot(X)
+fl <- createFolds(X[,1], k = 10, list = TRUE, returnTrain = FALSE)
+
+#Looping para os 10 testes
+acuracia <- NULL
+for (j in 1:10){
+  test <- X[fl[[j]],]
+  train <- X[-fl[[j]],]
 
 #Definindo constantes do teste
-errado <- 5
+errado <- 1
 k<-4
 
-while (errado > 2 && k < 40){
+#k-means
+while (errado != 0 && k < 40){
   errado <- 0
   k <- k+1
   
-  #Inicializando variaveis
-  iseq <- sample(length(X$x[,1]), k)
-  centroid <- X$x[iseq,]
-  oldc <- centroid+1
-  distnow <- NULL
-  distancia <- NULL
-  iteracao <- 0
-  minimos <- NULL
-  newc<-NULL
-  classe <- list()
-  
-  #Algoritmo k-means
-  while(all.equal(newc, centroid) != TRUE){
-    iteracao <- iteracao+1
-    if (iteracao == 100) break
-    for (i in 1:length(X$x[,1])){
-      for (j in 1:k){
-        distnow[j] <- eucdist(X$x[i,],centroid[j,])
-      }
-      distancia <- rbind(distancia,distnow)
-    }
-    
-    for (i in 1:(nc)){
-      minimos[i] <- which(distancia[i,] == min(distancia[i,]))
-    }
-    
-    newc <- centroid
-    for (i in 1:k){
-      pos <- NULL
-      pos <- which(minimos == i)
-      if (!is.null(pos)){
-        for (j in 1:length(X$x[1,])){
-          centroid[i,j] <- mean(X$x[pos,j])
-        }
-      } else {
-        centroid[i] <- X$x[sample(1:length(X$x[,1]),1),]
-      }
-    }
-  }
-  
-  pronto <- cbind(X$x,minimos)
-  
-  classe <- list()
-  cores <- rainbow(k)
+  w <- kmeans(train[,1:2],k,iter.max = 20)
+  media <- NULL
+
   for (i in 1:k){
-    classe[[i]] <- pronto[which(pronto[,3] == i),] 
+    newseq <- which(w$cluster==i)
+    media <- mean(as.numeric(train[newseq,3]))
+    if (media > as.numeric(train[newseq[1],3])*1.02 || media < as.numeric(train[newseq[1],3])*0.98) errado <- errado + 1
   }
-  
-  pronto <- cbind(pronto, X$classes)
-  
-  for (i in 1:k){
-    newseq <- which(pronto[,3]==i)
-    media <- mean(pronto[newseq,4])
-    cat("media: ",media,",era pra dar: ", as.numeric(X$classes[newseq[1]]), "\n")
-    if (media > pronto[newseq[1],4]*1.05 || media < pronto[newseq[1],4]*0.95) errado <- errado + 1
-  }
-  cat("k = ", k,"e errados:",errado, "\n\n\n")
 }
+
+cores <- rainbow(k)
+#####################################
+
+#funcao para estimativa da densidade de 2 variáveis
+pdf2var<-function(x,y,u1,u2,s1,s2,p) {(1/(2*pi*s1*s2*sqrt(1-(p^2))))*
+    exp((-(1)/(2*(1-(p^2))))*((((x-u1)^2)/((s1)^2))+(((y-u2)^2)/((s2)^2))-
+                                ((2*p*(x-u1)*(y-u2))/(s1*s2))))
+}
+
+#cáculo da média e desvio padrão das classes
+clusters <- list()
+c1 <- list()
+c2 <- list()
+count1 <- 1
+count2 <- 1
 for (i in 1:k){
-  plot(classe[[i]][,1], classe[[i]][,2], col=cores[i],  xlim = c(-1,1),ylim = c(-1,1),
-       xlab = '' ,ylab= '' )
-  par(new="T")
+  seqc <- which(w$cluster==i)
+  clusters[[i]] <- train[seqc,]
+  if (train[seqc[1],3]==1){
+    c1[[count1]] <- train[seqc,]
+    count1 <- count1+1
+  } else {
+    c2[[count2]] <- train[seqc,]
+    count2 <- count2+1
+  }
 }
-par(new="F")
+
+s1 <- list()
+s2 <- list()
+pc1 <- list()
+pc2 <- list()
+for (i in 1:length(c1)){
+  s1[[i]] <- cbind(sd(c1[[i]][,1]),sd(c1[[i]][,2]))
+  pc1[[i]] <- cov(c1[[i]][,1],c1[[i]][,2])/sqrt(prod(s1[[i]]^2))
+}
+for (i in 1:length(c2)){
+  s2[[i]] <- cbind(sd(c2[[i]][,1]),sd(c2[[i]][,2]))
+  pc2[[i]] <- cov(c2[[i]][,1],c2[[i]][,2])/sqrt(prod(s2[[i]]^2))
+}
+
+#Classificando as amostras de teste
+totalc1 <- NULL
+totalc2 <- NULL
+for (m in 1:length(c1)){
+  totalc1 <- c(totalc1,length(c1[[m]][,1]))
+}
+for (m in 1:length(c2)){
+  totalc2 <- c(totalc2,length(c2[[m]][,1]))
+}
+totalc1 <- sum(totalc1)
+totalc2 <- sum(totalc2)
+
+erro <- 0
+for (i in 1:length(test[,1])){
+  c <- 0
+  l <- t(test[i,1:2])
+  f1 <- NULL
+  f2 <- NULL
+  for (m in 1:length(c1)){
+  pir <- length(c1[[m]][,1])/totalc1
+  f1<- rbind(f1,pir*pdf2var(l[,1],l[,2],mean(c1[[m]][,1]),mean(c1[[m]][,2]),s1[[m]][1,1],s1[[m]][1,2],pc1[[m]]))
+  }
+  for (m in 1:length(c2)){
+    pir <- length(c2[[m]][,1])/totalc2
+    f2<- rbind(f2,pir*pdf2var(l[,1],l[,2],mean(c2[[m]][,1]),mean(c2[[m]][,2]),s2[[m]][1,1],s2[[m]][1,2],pc2[[m]]))
+  }
+  f1 <- sum(f1)
+  f2 <- sum(f2)
+  if (f2 == 0){
+    c <- 1
+  } else {
+    c <- if ((f1/f2 > 1) == TRUE) 1 else 2
+    if ((c-test[i,3]) != 0) erro <- erro +1
+  }
+}
+
+acuracia <- c(acuracia,(100*(1-(erro/length(test[,3])))))
+}
+
+mean(acuracia)
+sd(acuracia)
+
+#estimando as densidades de um grid
+seqi<-seq(-0.98,1,0.02)
+seqj<-seq(-0.98,1,0.02)
+M <- matrix(0,nrow=length(seqi),ncol=length(seqj)) 
+c<-NULL
+ci <- 0
+for (i in seqi){
+  ci <- ci+1
+  cj <- 0
+  for (j in seqj){
+    cj <- cj+1
+    f1 <- NULL
+    f2 <- NULL
+    for (m in 1:length(c1)){
+      pir <- length(c1[[m]][,1])/totalc1
+      f1<- rbind(f1,pir*pdf2var(i,j,mean(c1[[m]][,1]),mean(c1[[m]][,2]),s1[[m]][1,1],s1[[m]][1,2],pc1[[m]]))
+    }
+    for (m in 1:length(c2)){
+      pir <- length(c2[[m]][,1])/totalc2
+      f2<- rbind(f2,pir*pdf2var(i,j,mean(c2[[m]][,1]),mean(c2[[m]][,2]),s2[[m]][1,1],s2[[m]][1,2],pc2[[m]]))
+    }
+    f1 <- sum(f1)
+    f2 <- sum(f2)
+    if (f2 == 0){
+      c <- 1
+    } else {
+      c <- if ((f1/f2 > 1) == TRUE) 1 else 2
+    }
+    M[ci,cj] <- c
+  }
+}
+
+
+png("fig1.png", width = 500, height = 500)
+plot(c2[[]][,1:2], main="Dados de Entrada", xlim = c(-1,1),ylim = c(-1,1), xlab="x1", ylab="x2", col="red")
+dev.off()
+
+png("fig2.png", width = 500, height = 500)
+contour2D(x=seqi, y=seqj, z=M, col='blue', xlim = c(-1,1),ylim = c(-1,1), xlab="x1", ylab="x2")
+par(new=T)
+plot(X, main="Superfície de Separação", xlim = c(-1,1),ylim = c(-1,1), xlab="x1", ylab="x2")
+dev.off()
+
+sd(acuracia)
+
+image2D(M, x = seqi, y=seqj, col=c("black", "red"), 
+        colkey=(plot=FALSE),xlab = '' ,ylab= '', xlim = c(-1,1),ylim = c(-1,1), xaxs='i', yaxs='i' )
