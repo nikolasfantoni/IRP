3a24c52d18fc4021765639a86e92086efce1c32f
nikolasfantoni <nikolasfantoni@gmail.com>
2019-09-30 03:39:42 -0300

    Terminada aula 14

--- Aula14/Aula14.R
+++ Aula14/Aula14.R
@@ -32,7 +32,7 @@ for (i in 1:length(X$x[,1]))
 X$classes <- classes
 X <- cbind(X$x,X$classes)
 fl <- createFolds(X[,1], k = 10, list = TRUE, returnTrain = FALSE)
-rm(i)
+rm(i,classes,nc)
 
 
 acuracia <- NULL
@@ -40,7 +40,7 @@ for (ii in 1:10){
   test <- X[fl[[ii]],]
   train <- X[-fl[[ii]],]
 
-svmtrain <- ksvm(x =train[,1:2],y=train[,3],type='C-bsvc',kernel='rbfdot',kpar=list(sigma=0.5),C=2)
+svmtrain <- ksvm(x =train[,1:2],y=train[,3],type='C-bsvc',kernel='rbfdot',kpar=list(sigma=0.8),C=20)
 p <- NULL
 for (jj in 1:length(test[,1])){
   p <- predict(svmtrain,test[,-3])
@@ -48,6 +48,7 @@ for (jj in 1:length(test[,1])){
 d <-table(p,test[,3])
 acuracia <- c(acuracia,sum(diag(d))/sum(d))
 }
+acuracia <- 100*acuracia
 
 #estimando as densidades de um grid
 seqi<-seq(-0.995,1,0.005)
@@ -59,11 +60,25 @@ for (jj in 1:length(seqi)){
 }
 
 #plotando
-#plot(X, main="Dados de Entrada", xlim = c(-1,1),ylim = c(-1,1), xlab="x1", ylab="x2")
+
+#entrada
+plot(Y, xlim = c(-1,1),ylim = c(-1,1), xlab="x1", ylab="x2")
+
+#Vetores de Suporte
+indexes <- SVindex(svmtrain)
+plot(Y, xlim = c(-1,1),ylim = c(-1,1), xlab="x1", ylab="x2")
+par(new=T)
+plot(train[indexes,1],train[indexes,2], col = 'green', xlim = c(-1,1),ylim = c(-1,1), xlab="x1", ylab="x2")
 
 #Plotando Superficie de Separacao
-plot(Y, main="Superfície de Separação", xlim = c(-1,1),ylim = c(-1,1), xlab="x1", ylab="x2")
+plot(Y, xlim = c(-1,1),ylim = c(-1,1), xlab="x1", ylab="x2")
 par(new=T)
-contour(x=seqi, y=seqj, z=p, col='blue', xlim = c(-1,1),ylim = c(-1,1), xlab="x1", ylab="x2")
+contour(x=seqi, y=seqj, z=p, col='blue', xlim = c(-1,1),ylim = c(-1,1), xlab="x1", ylab="x2", levels = 1:2, labels = "", add = TRUE)
+
+#Superficie de Separacao 3D
+persp3D(seqi,seqj,p,counter=T,theta = 55, phi = 30, r = 40,
+        d = 0.1, expand = 0.5, ltheta = 90, lphi = 180, shade = 0.4,
+        ticktype = "detailed", nticks=5)
 
 #cat(acuracia,"\n")
+cat(mean(acuracia))
